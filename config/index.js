361888d186c5100c5323ac6306ba57ea792ece52
lidonglin <lidonglin.dl@bytedance.com>
2022-08-08 16:10:45 +0800

    feat: prod 环境生成 sourcemap

--- config/index.js
+++ config/index.js
@@ -24,6 +24,8 @@ const config = {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   mini: {
+    enableSourceMap: true,
+    sourceMapType: process.env.NODE_ENV === 'development' ? 'cheap-module-source-map' : 'source-map',
     postcss: {
       pxtransform: {
         enable: true,
